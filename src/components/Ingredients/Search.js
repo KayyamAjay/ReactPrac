a979146d40dc62af0190f58ed51f6316fcab6c85
Ajay Kayyam <saiajay220@gmail.com>
2022-01-01 11:42:55 +0530

    clean up UseEffect

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -6,29 +6,37 @@ import "./Search.css";
 const Search = React.memo((props) => {
   const [filteredIngredients, setfilteredIngredients] = React.useState("");
   const { onLoading } = props;
+  const inputRef = React.useRef();
   React.useEffect(() => {
-    const fetchHandler = async () => {
-      const query =
-        filteredIngredients.length === 0
-          ? ""
-          : `?orderBy="title"&equalTo="${filteredIngredients}"`;
-      const response = await fetch(
-        "https://sample-26d34-default-rtdb.firebaseio.com/ingredients.json" +
-          query
-      );
-      const data = await response.json();
-      const loadingList = [];
-      for (const key in data) {
-        loadingList.push({
-          id: key,
-          title: data[key].title,
-          amount: data[key].amount,
-        });
+    const timer = setTimeout(() => {
+      if (filteredIngredients === inputRef.current.value) {
+        const fetchHandler = async () => {
+          const query =
+            filteredIngredients.length === 0
+              ? ""
+              : `?orderBy="title"&equalTo="${filteredIngredients}"`;
+          const response = await fetch(
+            "https://sample-26d34-default-rtdb.firebaseio.com/ingredients.json" +
+              query
+          );
+          const data = await response.json();
+          const loadingList = [];
+          for (const key in data) {
+            loadingList.push({
+              id: key,
+              title: data[key].title,
+              amount: data[key].amount,
+            });
+          }
+          onLoading(loadingList);
+        };
+        fetchHandler();
       }
-      onLoading(loadingList);
-    };
-    fetchHandler();
-  }, [filteredIngredients, onLoading]);
+      return () => {
+        clearTimeout(timer);
+      };
+    }, 500);
+  }, [filteredIngredients, onLoading, inputRef]);
 
   return (
     <section className="search">
@@ -36,6 +44,7 @@ const Search = React.memo((props) => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             onChange={(event) => setfilteredIngredients(event.target.value)}
           />
