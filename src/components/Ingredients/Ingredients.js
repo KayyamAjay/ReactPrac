778e3127917366ba3d4f3ecb054b90b555e18a35
Ajay Kayyam <saiajay220@gmail.com>
2022-01-02 09:33:16 +0530

    Adding Loading and error states

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,14 +2,17 @@ import React, { useState, useCallback } from "react";
 import IngreditenList from "./IngredientList";
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
+import ErrorModal from "../UI/ErrorModal";
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
-
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const filter = useCallback((filteredingredients) => {
     setUserIngredients(filteredingredients);
   }, []);
   const addIngredientsHandler = async (ingredient) => {
+    setLoading(true);
     const response = await fetch(
       "https://sample-26d34-default-rtdb.firebaseio.com/ingredients.json",
       {
@@ -19,30 +22,41 @@ function Ingredients() {
       }
     );
     const data = await response.json();
-
+    setLoading(false);
     setUserIngredients((prev) => [
       ...prev,
       { id: data.name, ...ingredient }, //adding id and keeping the key value pair by ...ingredient
     ]);
   };
   const removeIngredientHandler = async (id) => {
-    const response = await fetch(
-      `https://sample-26d34-default-rtdb.firebaseio.com/ingredients/${id}.json`,
-      {
-        method: "DELETE",
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://sample-26d34-default-rtdb.firebaseio.com/ingredients/${id}.json`,
+        {
+          method: "DELETE",
+        }
+      );
+      setLoading(false);
+      if (response.ok) {
+        console.log("deleted the selected item");
       }
-    );
-    if (response.ok) {
-      console.log("deleted the selected item");
+      setUserIngredients((prev) =>
+        prev.filter((ingredient) => ingredient.id !== id)
+      );
+    } catch (err) {
+      setError("something went wrong!");
+      setLoading(false);
     }
-    setUserIngredients((prev) =>
-      prev.filter((ingredient) => ingredient.id !== id)
-    );
+    console.log(userIngredients);
   };
-  console.log(userIngredients);
+  const clearError = () => {
+    setError(null);
+  };
   return (
     <div className="App">
-      <IngredientForm onAdd={addIngredientsHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm onLoading={isLoading} onAdd={addIngredientsHandler} />
 
       <section>
         <Search onLoading={filter} />
